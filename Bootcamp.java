21301db8454ad4e974fcde35b5082f1feff945b0
Pranav Kumar <pranavkumar.dugutta@gmail.com>
2026-02-04 09:24:19 +0530

    Add files via upload

--- Bootcamp.java
+++ Bootcamp.java
@@ -557,28 +557,294 @@ public class Bootcamp{
         System.out.println("Result = " + s.pop());
     }
 }
-*/
+*//*
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.Arrays;
+public class Bootcamp {
+	static int MAX_SIZE = 100;
+	static int[] stack = new int[MAX_SIZE];
+	static int top = -1;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int choice;
+		do {
+			System.out.println("\n--- Stack Operations ---");
+			System.out.println("1. Push");
+			System.out.println("2. Pop");
+			System.out.println("3. Display");
+			System.out.println("4. Exit");
+			System.out.print("Enter your choice: ");
+			choice = sc.nextInt();
+			switch (choice) {
+				case 1:
+					System.out.print("Enter value to push: ");
+					int value = sc.nextInt();
+					push(value);
+					break;
+				case 2:
+					pop();
+					break;
+				case 3:
+					display();
+					break;
+				case 4:
+					System.out.println("Exiting...");
+					break;
+				default:
+					System.out.println("Invalid choice!");
+			}
+		} 
+        while (choice != 4);
+		sc.close();
+	}
+	static boolean isFull() {
+		return top == MAX_SIZE - 1;
+	}
+	static boolean isEmpty() {
+		return top == -1;
+	}
+	static void push(int value) {
+		if (isFull()) {
+			System.out.println("Stack Overflow! Cannot push " + value);
+			return;
+		}
+		stack[++top] = value;
+		System.out.println(value + " pushed to stack");
+	}
+	static int pop() {
+		if (isEmpty()) {
+			System.out.println("Stack Underflow! Cannot pop");
+			return -1;
+		}
+		System.out.println(stack[top] + " popped from stack");
+		return stack[top--];
+	}
+	static void display() {
+		if (isEmpty()) {
+			System.out.println("Stack is empty!");
+			return;
+		}
+		System.out.print("Stack: ");
+		for (int i = 0; i <= top; i++) {
+			System.out.print(stack[i] + " ");
+		}
+		System.out.println();
+	}
+}
+*//*
+//Queue : A queue is a linear data structure that follows first-in first-out principle. 
+//linear Queue : 
+import java.util.Scanner;
 public class Bootcamp{
+    private int[] arr;
+    private int front;
+    private int rear;
+    private int capacity;
+    private int count;
+    public Bootcamp(int size){
+        capacity = size;
+        arr = new int[capacity];
+        front = 0;
+        rear = -1;
+        count = 0;
+    }
+    public void enqueue(int item){
+        if(isFull()){
+            System.out.println("Queue is full. Can't enqueue "+ item);
+            return;
+        }
+        arr[++rear] = item;
+        count++;
+        System.out.println(item +" enqueued to queue.");
+    }
+    public void dequeue(){
+        if(isEmpty()){
+            System.out.println("Queue is empty.Can't dequeue.");
+            return;
+        }
+        int item = arr[front++];
+        count--;
+        System.out.println(item +" dequeued from queue.");
+    }
+    public void peek(){
+        if(isEmpty()){
+            System.out.println("Queue is empty.");
+            return;
+        }
+        System.out.println("Front element is : "+ arr[front]);
+    }
+    public void display(){
+        if(isEmpty()){
+            System.out.println("Queue is empty.");
+            return;
+        }
+        System.out.println("Queue Elements : ");
+        for(int i = 0; i < count; i++){
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public boolean isEmpty(){
+        return count == 0;
+    }
+    public boolean isFull(){
+        return count == capacity;
+    }
     public static void main(String[] args){
-        int[] arr = new int[100];
-        int top = -1;
-        boolean isempty(){
-            return top == -1;
-        }
-        boolean isFull(){
-            return top == arr.length - 1;
-        }
-        void push(int data){
-            if(isFull()){
-                System.out.println("Stack is full");
-                return;
-            }
-            top++;
-            arr[top] = data;
-            System.out.println(data+" pushed to stack.");s
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter queue size : ");
+        int capacity = sc.nextInt();
+        Bootcamp queue = new Bootcamp(capacity);
+        int choice;
+         do{
+            System.out.println("\n--- Queue Operations ---");
+            System.out.println("1. Enqueue");
+			System.out.println("2. Dequeue");
+			System.out.println("3. peek");
+			System.out.println("4. Display");
+            System.out.println("5. Is full");
+            System.out.println("6. Is empty");
+            System.out.println("7. Exit");
+			System.out.print("Enter your choice: ");
+			choice = sc.nextInt();
+            switch(choice){
+                case 1:
+                    System.out.println("Enter element to enqueue : ");
+                    int element = sc.nextInt();
+                    queue.enqueue(element);
+                    break;
+                case 2:
+                    queue.dequeue();
+                    break;
+                case 3:
+                    queue.peek();
+                    break;
+                case 4:
+                    queue.display();
+                    break;
+                case 5:
+                    System.out.println(queue.isFull() ? "The queue is full." : "The queue is not full.");
+                    break;
+                case 6:
+                    System.out.println(queue.isEmpty() ? "The queue is empty." : "The queue is not empty.");
+                    break;            
+                case 7:
+                    System.out.println("Exititng .....");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+			}
+		} 
+        while (choice != 7);
+		sc.close();
     }
 }
+*//*
+//Circular Queue:
+import java.util.Scanner;
+public class Bootcamp{
+    private int[] arr;
+    private int front;
+    private int rear;
+    private int capacity;
+    private int count;
+    public Bootcamp(int size){
+        capacity = size;
+        arr = new int[capacity];
+        front = 0;
+        rear = -1;
+        count = 0;
+    }
+    public void enqueue(int item){
+        if(isFull()){
+            System.out.println("Queue is full. Can't enqueue "+ item);
+            return;
+        }
+        rear = (rear + 1) % capacity;
+        arr[rear] = item;
+        count++;
+        System.out.println(item +" enqueued to queue.");
+    }
+    public void dequeue(){
+        if(isEmpty()){
+            System.out.println("Queue is empty.Can't dequeue.");
+            return;
+        }
+        int item = arr[front];
+        front = (front + 1) % capacity;
+        count--;
+        System.out.println(item +" dequeued from queue.");
+    }
+    public void peek(){
+        if(isEmpty()){
+            System.out.println("Queue is empty.");
+            return;
+        }
+        System.out.println("Front element is : "+ arr[front]);
+    }
+    public void display(){
+        if(isEmpty()){
+            System.out.println("Queue is empty.");
+            return;
+        }
+        System.out.println("Queue Elements : ");
+        for(int i = 0; i < count; i++){
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public boolean isEmpty(){
+        return count == 0;
+    }
+    public boolean isFull(){
+        return count == capacity;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter queue size : ");
+        int capacity = sc.nextInt();
+        Bootcamp queue = new Bootcamp(capacity);
+        int choice;
+         do{
+            System.out.println("\n--- Queue Operations ---");
+            System.out.println("1. Enqueue");
+			System.out.println("2. Dequeue");
+			System.out.println("3. peek");
+			System.out.println("4. Display");
+            System.out.println("5. Is full");
+            System.out.println("6. Is empty");
+            System.out.println("7. Exit");
+			System.out.print("Enter your choice: ");
+			choice = sc.nextInt();
+            switch(choice){
+                case 1:
+                    System.out.println("Enter element to enqueue : ");
+                    int element = sc.nextInt();
+                    queue.enqueue(element);
+                    break;
+                case 2:
+                    queue.dequeue();
+                    break;
+                case 3:
+                    queue.peek();
+                    break;
+                case 4:
+                    queue.display();
+                    break;
+                case 5:
+                    System.out.println(queue.isFull() ? "The queue is full." : "The queue is not full.");
+                    break;
+                case 6:
+                    System.out.println(queue.isEmpty() ? "The queue is empty." : "The queue is not empty.");
+                    break;            
+                case 7:
+                    System.out.println("Exititng .....");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+			}
+		} 
+        while (choice != 7);
+		sc.close();
+    }
+}
+*/
