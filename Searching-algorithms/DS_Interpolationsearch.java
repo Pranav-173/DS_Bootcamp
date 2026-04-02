c9cc70e16c7cd94d2a83c540963541f635749150
Amal Bijoy <amalbijoy2007@gmail.com>
2026-04-02 15:09:15 +0530

    Refactor input/output messages in interpolation search

--- Searching-algorithms/DS_Interpolationsearch.java
+++ Searching-algorithms/DS_Interpolationsearch.java
@@ -1,25 +1,24 @@
-// Interpolation search :
 import java.util.Scanner;
 import java.util.Arrays;
-public class DS_Interpolationsearch{
-    public static void main(String[] args){
+
+public class DS_Interpolationsearch {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the length of the array : ");
+        System.out.println("Enter the length of the Array: ");
         int n = sc.nextInt();
-        System.out.println("Enter array elements : ");
+        System.out.println("Enter Array elements: ");
         int arr[] = new int[n];
-        for(int i = 0 ; i < n;i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("Entered array : "+ Arrays.toString(arr));
-        System.out.println("Enter the key element to be found : ");
+        System.out.println("Entered Array: " + Arrays.toString(arr));
+        System.out.println("Enter the key element to be Found: ");
         int key = sc.nextInt();
         int result = interpolationSearch(arr, 0, arr.length - 1, key);
-        if(result != -1){
-            System.out.println("Element "+ key +" found at index : "+ result);
-        }
-        else{
-            System.out.println("Element "+ key +" was not found in the entered array.");
+        if (result != -1) {
+            System.out.println("Element " + key + " Found at Index: " + result);
+        } else {
+            System.out.println("Element " + key + " was NOT Found in the entered Array.");
         }
     }
     public static int interpolationSearch(int arr[], int lo, int hi, int x) {
