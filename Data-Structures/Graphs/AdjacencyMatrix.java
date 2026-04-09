1bb5e920786caf0764a5960ee05717c0d2e4b834
Amal Bijoy <amalbijoy2007@gmail.com>
2026-04-09 10:34:02 +0530

    Update AdjacencyMatrix.java

--- Data-Structures/Graphs/AdjacencyMatrix.java
+++ Data-Structures/Graphs/AdjacencyMatrix.java
@@ -6,21 +6,18 @@ public class AdjacencyMatrix {
             );
         }
     }
-
     public static void addEdge(int[][] mat, int i, int j) {
         validateVertex(mat, i, "Source vertex");
         validateVertex(mat, j, "Destination vertex");
         mat[i][j] = 1;
         mat[j][i] = 1; // for undirected graph
     }
-
     public static void removeEdge(int[][] mat, int i, int j) {
         validateVertex(mat, i, "Source vertex");
         validateVertex(mat, j, "Destination vertex");
         mat[i][j] = 0;
         mat[j][i] = 0; // for undirected graph
     }
-
     public static void displayMatrix(int[][] mat) {
         for (int[] row : mat) {
             for (int val : row) {
@@ -29,22 +26,17 @@ public class AdjacencyMatrix {
             System.out.println();
         }
     }
-
     public static void main(String[] args) {
         int v = 4;
         int[][] mat = new int[v][v];
-
         addEdge(mat, 0, 1);
         addEdge(mat, 0, 2);
         addEdge(mat, 1, 2);
         addEdge(mat, 2, 3);
-
         System.out.println("Adjacency Matrix after adding edges:");
         displayMatrix(mat);
-
         removeEdge(mat, 1, 2);
         removeEdge(mat, 2, 3);
-
         System.out.println("Adjacency Matrix after removing edges (1,2) and (2,3):");
         displayMatrix(mat);
     }
