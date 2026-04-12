1e922ec052b0dd9845123c3c6591c0b26d3d4c9b
Amal Bijoy <amalbijoy2007@gmail.com>
2026-04-13 00:31:01 +0530

    Update and rename Data-Structures/Queue/BoundedQueue.java to Data-Structures/Queue/BoundedQueue/BoundedQueue.java

--- Data-Structures/Queue/BoundedQueue/BoundedQueue.java
+++ Data-Structures/Queue/BoundedQueue/BoundedQueue.java
@@ -5,33 +5,26 @@ public class BoundedQueue {
 	private static void printCheck(String label, Object expected, Object got) {
 		System.out.println(label + " -> Expected: " + expected + " | Got: " + got);
 	}
-
 	public static void main(String[] args) {
 		ArrayBlockingQueue<Integer> boundedQueue = new ArrayBlockingQueue<>(2);
-
-		printCheck("Initial queue", "[]", boundedQueue.toString());
-
+		printCheck("Initial Queue", "[]", boundedQueue.toString());
 		boundedQueue.add(10);
 		boundedQueue.add(20);
 		printCheck("After adding 10, 20", "[10, 20]", boundedQueue.toString());
-
 		boolean inserted = boundedQueue.offer(30);
-		printCheck("offer(30) when full", false, inserted);
-
-		printCheck("peek() on non-empty queue", 10, boundedQueue.peek());
-		printCheck("poll() on non-empty queue", 10, boundedQueue.poll());
+		printCheck("offer(30) when Full", false, inserted);
+		printCheck("peek() on NON-Empty queue", 10, boundedQueue.peek());
+		printCheck("poll() on NON-Empty queue", 10, boundedQueue.poll());
 		printCheck("Queue after poll()", "[20]", boundedQueue.toString());
-		printCheck("remove() on non-empty queue", 20, boundedQueue.remove());
+		printCheck("remove() on NON-Empty Queue", 20, boundedQueue.remove());
 		printCheck("Queue after remove()", "[]", boundedQueue.toString());
-
-		printCheck("peek() on empty queue", null, boundedQueue.peek());
-		printCheck("poll() on empty queue", null, boundedQueue.poll());
-
+		printCheck("peek() on Empty Queue", null, boundedQueue.peek());
+		printCheck("poll() on Empty Queue", null, boundedQueue.poll());
 		try {
 			boundedQueue.remove();
-			printCheck("remove() on empty queue", "NoSuchElementException", "No exception");
+			printCheck("remove() on Empty Queue", "NoSuchElementException", "No exception");
 		} catch (NoSuchElementException ex) {
-			printCheck("remove() on empty queue", "NoSuchElementException", ex.getClass().getSimpleName());
+			printCheck("remove() on Empty Queue", "NoSuchElementException", ex.getClass().getSimpleName());
 		}
 	}
 }
